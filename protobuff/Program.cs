7ff7db557549346aeb73f0f54084106c51b5265b
shakedt1 <49534411+shakedt1@users.noreply.github.com>
2021-04-13 12:29:10 +0300

    Add multyple messages serialization and deserialization feature

--- protobuff/Program.cs
+++ protobuff/Program.cs
@@ -2,13 +2,14 @@
 using System.Reflection;
 using System.IO;
 using Google.Protobuf;
-using AnyClass;
 using Google.Protobuf.WellKnownTypes;
 
-namespace Person
+
+namespace Generic
 {
     class Program
     {
+        public const string fileName = "Person.dat";
         // Convert string name of class to type object
         public static System.Type GetType(string typeName)
         {
@@ -38,7 +39,7 @@ namespace Person
         }
 
         // Parse and unpack the any object
-        public static object readChildMessage(genericClass genericClass)
+        public static object readChildMessage(GenericClass genericClass)
         {
             try
             {
@@ -64,46 +65,96 @@ namespace Person
                 myObject = unpackMethod.Invoke(childMessage, null);
                 return myObject;
             }
-            catch (InvalidProtocolBufferException e)
+            catch (Exception ex) when (ex is InvalidProtocolBufferException || ex is NullReferenceException)
             {
-                e.GetType();
                 return null;
             }
 
         }
-        static void Main(string[] args)
+
+        // Get a message and write it to a file
+        public static void writeToFile(IMessage message ,string fileName) 
         {
+            // Create out generic class that holds any other classes
+            GenericClass genericClass = new GenericClass 
+            { 
+                MyObject = Any.Pack(message) 
+            };
+
+            // Serialize the class and write it to a file
+            using (Stream output = File.Open(fileName, FileMode.Append))
+            {
+                MessageExtensions.WriteDelimitedTo(genericClass, output);
+            }
+        }
+
+        // Get a file name and read the first object 
+        public static GenericClass readFromFile(Stream input)
+        {
+            MessageParser<GenericClass> parser = new MessageParser<GenericClass>(() => { return new GenericClass(); });
+            try
+            {
+                // Deerialize the first object and return it as generic class
+                return parser.ParseDelimitedFrom(input);
+            }
+            // If there is no more objects
+            catch (InvalidProtocolBufferException) 
+            {
+                return null;
+            }
+            
+        }
+
+        static void Main()
+        {
+            using (File.Create(fileName));
+
             // Create a class, can be any class
+            PhoneNumber phoneNumber = new PhoneNumber
+            {
+                Prefix = "054",
+                Number = "7523111"
+            };
+
             Person person = new Person
             {
                 Id = 5,
                 Name = "shaked",
-                Emails = { "shake", "email" }
+                Emails = { "shake", "email" },
+                Number = phoneNumber
             };
 
-            // Create out generic class that holds any other classes
-            genericClass genericClass = new genericClass
+            Person meir = new Person
             {
-                MyObject = Any.Pack(person)
+                Id = 8,
+                Name = "meir",
+                Emails = { "meir", "gmail" }
             };
 
-            // Serialize the class and write it to a file
-            using (var output = File.Create("person.dat"))
+            Dog dog = new Dog
             {
-                genericClass.WriteTo(output);
-            }
+                Name = "Lucio",
+                Type = "Bulldog"
+            };
 
-            // Deserialize the file and read it to our generic class
-            genericClass parsedClass;
-            using (var input = File.OpenRead("person.dat"))
+            // Write our objects to a file
+            writeToFile(person, fileName);            
+            writeToFile(phoneNumber, fileName);
+            writeToFile(dog, fileName);
+            writeToFile(meir, fileName);
+
+            // Read objects from file. Parse and unpack the any object
+            using (Stream input = File.OpenRead(fileName))
             {
-                parsedClass = genericClass.Parser.ParseFrom(input);
+                object obPerson = readChildMessage(readFromFile(input));
+                object obPhone = readChildMessage(readFromFile(input));
+                object obDog = readChildMessage(readFromFile(input));
+                object obMeir = readChildMessage(readFromFile(input));
+                Console.WriteLine("type: {0}\nvalue: {1}\n", obPerson.GetType(), obPerson.ToString());
+                Console.WriteLine("type: {0}\nvalue: {1}\n", obPhone.GetType(), obPhone.ToString());
+                Console.WriteLine("type: {0}\nvalue: {1}\n", obDog.GetType(), obDog.ToString());
+                Console.WriteLine("type: {0}\nvalue: {1}\n", obMeir.GetType(), obMeir.ToString());
             }
-
-            // Parse and unpack the any object
-            object ob = readChildMessage(parsedClass);
-            Console.WriteLine("type: {0}\nvalue: {1}", ob.GetType(), ob.ToString());
-
         }
     }
 }
